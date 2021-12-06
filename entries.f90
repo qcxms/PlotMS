0692661cf14e3c70d74118aa690eb6c69bf945b2
JayTheDog <jeroen.koopman@thch.uni-bonn.de>
2021-12-06 17:49:31 +0100

    accurate charge update entries check

--- entries.f90
+++ entries.f90
@@ -20,6 +20,8 @@ module count_entries
 
       real(wp) :: list_masses(10000)
       real(wp) :: intensity(10000)
+
+      real(wp) :: mass_diff
     
       !real(wp), allocatable :: list_masses(:)
       !real(wp), allocatable :: save_list(:)
@@ -37,78 +39,41 @@ module count_entries
       !count_mass = count_mass + index_mass
       if(count_mass == 0)then
         sum_index =  index_mass
-      !  allocate ( list_masses(index_mass))
-      !  allocate ( intensity(index_mass))
       else
         sum_index = count_mass + index_mass
-      !  if(allocated(save_list)) write(*,*) 'JOP'
-      !  if(allocated(save_list))deallocate (save_list)
-      !  if(allocated(save_int))deallocate (save_int)
-      !  allocate ( save_list(count_mass))
-      !  allocate ( save_int (count_mass))
-
-      ! !write(*,*) 'COUNT MASS', count_mass
-      ! do i = 1, count_mass 
-      !  save_list(i) = list_masses(i)
-      !  save_int(i)  = intensity(i)
-      ! enddo
-
-      !  deallocate (list_masses)
-      !  deallocate (intensity)
-      !  allocate ( list_masses(sum_index))
-      !  allocate ( intensity(sum_index))
-
-      !   !write(*,*) 'save', save_list
-      !   !write(*,*) 'list', list_masses
-
-      !  do i = 1, count_mass
-      !    list_masses(i)  = save_list(i)
-      !    intensity(i)    = save_int(i)
-      !  enddo
       endif
 
 
-      !if (.not. allocated(list_masses)) allocate ( list_masses(sum_index))
-
-      !write(*,*) 'BUCKET'
-      !write(*,*) 'index', index_mass
-      !write(*,*) 'sum', sum_index
-      !write(*,*) 'count', count_mass
-
+      !> loop over the list from the isotope subroutine
 outer: do loop2 = 1, index_mass 
         loop = 0
-        !write(*,*) isotope_masses(loop2)
 
-        !write(*,*) 'LOOP2', loop2
-        do 
+        !> loop over all entries of new (check) list 
+inner:  do 
           loop = loop + 1
+          mass_diff = abs(list_masses(loop) - isotope_masses(loop2))
 
-          if  ( list_masses(loop) == isotope_masses(loop2) ) then
-          !write(*,*) 'LOOP', loop, 'TRUE'
+          if ( list_masses(loop) == 0.0_wp ) then
+            there = .false.
+             !write(*,*) 'NULL'
+            exit inner
+
+          !elseif  ( list_masses(loop) == isotope_masses(loop2) ) then
+          elseif  ( mass_diff < 1.0d0-10 .or. mass_diff == 0.0_wp) then
             there = .true.
-            !write(*,*) 'TRUE'
-            !write(*,*) 'intensity', loop,intensity(loop)
-            !intensity(loop) = intensity(loop) + 1
             intensity(loop) = intensity(loop) +  1 * exact_intensity(loop2) &
               * abs(chrg)
             if (loop2 < index_mass ) cycle outer 
-            if (loop2 == index_mass ) exit 
+            if (loop2 == index_mass ) exit inner
 
-          elseif ( list_masses(loop) == 0.0_wp ) then
-            there = .false.
-             !write(*,*) 'NULL'
-            exit
 
           !>> false if not in list, store
-          elseif ( list_masses(loop) /= isotope_masses(loop2) ) then
-          !write(*,*) 'LOOP', loop, 'FALSE'
+          !elseif ( list_masses(loop) /= isotope_masses(loop2) ) then
+          elseif  ( mass_diff > 1.0d0-10 ) then
             there = .false.
-            !write(*,*) 'FALSE'
-            !write(*,*) loop, loop2
-            !if (loop == count_mass ) exit
-            if (loop == sum_index ) exit
+            if (loop == sum_index ) exit inner
           endif
-        enddo
+        enddo inner
 
 
         if ( .not. there ) then
@@ -120,45 +85,6 @@ outer: do loop2 = 1, index_mass
 
       enddo outer
 
-      !if(allocated(intensity) .and. allocated(save_int)) then
-      !  deallocate (save_int)
-      !  allocate (save_int(count_mass))
-
-      !  do i = 1, count_mass 
-      !    save_int(i)=intensity(i)
-      !   enddo
-
-      !    deallocate (intensity)
-      !    allocate ( intensity(count_mass))
-
-      !    do i = 1, count_mass 
-      !      intensity(i)    = save_int(i)
-      !    enddo
-      !endif
-
-      !if(allocated(list_masses) .and. allocated(save_list)) then
-      !  deallocate (save_list)
-      !  allocate (save_list(count_mass))
-
-      !  do i = 1, count_mass 
-      !    save_list(i)=list_masses(i)
-      !   enddo
-
-      !    deallocate (list_masses)
-      !    allocate ( list_masses(count_mass))
-
-      !    do i = 1, count_mass 
-      !      list_masses(i)    = save_list(i)
-      !    enddo
-      !  write(*,*) 
-      !endif
-      !deallocate (save_list)
-      !deallocate (save_int)
-
-      !    do i = 1, count_mass 
-        !    write(*,*) i,list_masses(i),intensity(i)
-            !write(*,*) i,intensity(i)
-       !   enddo
    
     end subroutine check_entries
     
