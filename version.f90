a3cb1e2a7bcfa8b201f26c002bbe7e6eea14dfc8
JayTheDog <jeroen.koopman@thch.uni-bonn.de>
2022-03-01 17:05:44 +0100

    matching score re-implemented with integer precision

---
+++ version.f90
@@ -0,0 +1,23 @@
+module version
+  implicit none
+
+  contains
+
+subroutine print_version
+
+  write(*,*)
+  write(*,'(6x,''* * * * * * * * * * * * * * * * * *'')')
+  write(*,'(6x,''*           P l o t M S           *'')')
+  write(*,'(6x,''* * * * * * * * * * * * * * * * * *'')')
+  write(*,'(6x,''*    QCxMS spectra plotting tool  *'')')
+  write(*,'(6x,''*         -  v. 6.2.0  -          *'')')
+  write(*,'(6x,''*          01. Mar 2022           *'')')
+  write(*,'(6x,''*           *   *   *             *'')')
+  write(*,'(6x,''*           S. Grimme             *'')')
+  write(*,'(6x,''*           J. Koopman            *'')')
+  write(*,'(6x,''* * * * * * * * * * * * * * * * * *'')')
+  write(*,'(6x,''   Contributor:  C.Bauer, T.Kind   '')')
+  write(*,*)
+
+end subroutine print_version
+end module version
