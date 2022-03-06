daecb45e76459a26300a1f8768ede1030c5414a9
JayTheDog <jeroen.koopman@thch.uni-bonn.de>
2022-03-06 13:49:37 +0100

    restructured build; included mctc-lib

--- src/readcommon.f90
+++ src/readcommon.f90
@@ -1,8 +1,10 @@
-module qcxms_readcommon
+!module qcxms_readcommon
+module readcommon
   use xtb_mctc_accuracy, only: wp
   implicit none
 
   integer           :: iocheck = 0
   character(len=80) :: line 
 
-end module qcxms_readcommon
+!end module qcxms_readcommon
+end module readcommon
